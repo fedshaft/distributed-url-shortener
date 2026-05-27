37909dddafa50df745ff5bc64b474a37ab95a5f3
fedshaft <adhav2006@gmail.com>
2026-05-27 11:44:42 +0530

    implemented retry loop to handle short_code collisions

--- main.py
+++ main.py
@@ -27,14 +27,24 @@ async def shorten_url (url: str):
     if not url.startswith("http://") and not url.startswith("https://"):
         return {"error": "Invalid URL format"}
     async with app.state.db_pool.acquire() as conn:
-        result = await conn.fetchrow("select short_code from urls where long_url = $1", url)
-        if result:
-            return {"shortened_url": result['short_code']}
-        shortened_url = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-        await conn.execute("insert into urls (long_url, short_code) values ($1, $2)", url, shortened_url)
-    return {"shortened_url": shortened_url}
-    
-        
+        try:
+            result = await conn.fetchrow ("select short_code from urls where long_url = $1", url)
+            if result:
+                return {'short_code': result['short_code']}
+            
+            max_tries = 5
+            for i in range(max_tries):
+                short_code = ''.join(random.choices(string.ascii_letters + string.digits, k = 6))
+                try:
+                    await conn.execute("insert into urls (long_url, short_code) values ($1, $2)", url, short_code)
+                    return {"short_code": short_code}
+                except asyncpg.UniqueViolationError:
+                    continue
+                except Exception as e:
+                    return {"error": str(e)}
+            return {"error": "failed to generate unique short code after multiple attempts"}
+            
+
 @app.get("/{shortened_url}")
 async def redirect(shortened_url: str):
     async with app.state.db_pool.acquire() as conn:
