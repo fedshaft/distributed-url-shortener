9a24a3e0901620515bdbd4153dfd3d25fb91ab61
fedshaft <adhav2006@gmail.com>
2026-05-28 10:52:51 +0530

    refactored connection pooling using dependency injection and switched redirect route to redirectresponse

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import RedirectResponse
 import random, string
 from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
 import asyncpg
+
 
 load_dotenv()
 @asynccontextmanager
@@ -22,33 +24,38 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
+async def get_conn(request: Request):
+    async with request.app.state.db_pool.acquire() as conn:
+        yield conn
+    
+
 @app.post("/shorten")
-async def shorten_url (url: str):
+async def shorten_url(url: str, conn = Depends(get_conn)):
     if not url.startswith("http://") and not url.startswith("https://"):
         return {"error": "Invalid URL format"}
-    async with app.state.db_pool.acquire() as conn:
-        try:
-            result = await conn.fetchrow ("select short_code from urls where long_url = $1", url)
-            if result:
-                return {'short_code': result['short_code']}
-            
-            max_tries = 5
-            for i in range(max_tries):
-                short_code = ''.join(random.choices(string.ascii_letters + string.digits, k = 6))
-                try:
-                    await conn.execute("insert into urls (long_url, short_code) values ($1, $2)", url, short_code)
-                    return {"short_code": short_code}
-                except asyncpg.UniqueViolationError:
-                    continue
-                except Exception as e:
-                    return {"error": str(e)}
-            return {"error": "failed to generate unique short code after multiple attempts"}
-            
+    try:
+        result = await conn.fetchrow("select short_code from urls where long_url = $1",url)
+        if result:
+            return {"short_code": result["short_code"]}
+        max_tries = 5
+        for _ in range(max_tries):
+            short_code = ''.join(random.choices(string.ascii_letters + string.digits,k=6))
+            try:
+                await conn.execute("insert into urls (long_url, short_code) values ($1, $2)", url,short_code)
+                return {"short_code": short_code}
+            except asyncpg.UniqueViolationError:
+                continue
+        return {"error": "failed to generate unique short code"}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/{shortened_url}")
-async def redirect(shortened_url: str):
-    async with app.state.db_pool.acquire() as conn:
-        result = await conn.fetchrow("select long_url from urls where short_code = $1", shortened_url)
-        if not result:
-            return {"error": "Shortened URL not found"}
-        return {"url": result['long_url']}
+async def redirect(shortened_url: str, conn = Depends(get_conn)):
+        try:
+            result = await conn.fetchrow("select long_url from urls where short_code = $1", shortened_url)
+            if result:
+                return RedirectResponse(result['long_url'])
+            else:
+                return {"error": "Short URL not found"}
+        except Exception as e:
+            return {"error": str(e)}
